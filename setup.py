e4e01ea1768c53fa8b63632b06e54d718ba3cc6a
eYokai <103901993+E-Doan@users.noreply.github.com>
2023-03-21 15:27:23 -0400

    Test- setup files not found, testing change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pcs',
+    name='pcs-lambda-framework',
     version='0.0.1',
     author='PCS',
     author_email='https://productivecloudsolutions.com',
