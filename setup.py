9c936dea32976b311728bd6edfacb16499b2f95e
eYokai <103901993+E-Doan@users.noreply.github.com>
2023-03-21 15:30:39 -0400

    Check fix for out of date code

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
 
 setuptools.setup(
     name='pcs-lambda-framework',
@@ -9,7 +8,6 @@ setuptools.setup(
     author='PCS',
     author_email='https://productivecloudsolutions.com',
     description='Installation of PCS Package',
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Productive-Cloud-Solutions/PCS-Lambda-Framework',
     project_urls = {
