6471b0205f9767323a4156382d07f26aab622145
eYokai <103901993+E-Doan@users.noreply.github.com>
2023-04-19 12:24:20 -0400

    Framework cleanup 2

--- pcs/models/stripeWebhook.py
+++
@@ -1,9 +0,0 @@
-from pcs.models.baseModel import BaseModel
-
-
-class StripeWebhook(BaseModel):
-
-    def __init__(self) -> None:
-
-        super().__init__("stripe-webhook-table")
-
--- pcs/models/user.py
+++
@@ -1,9 +0,0 @@
-from pcs.models.baseModel import BaseModel
-
-class User(BaseModel):
-
-    def __init__(self) -> None:
-
-        super().__init__("user-table")
-        #Must come after init or ELSE!!!
-
