d0b5201e90de37991a42e8477153f22d598b298f
eYokai <103901993+E-Doan@users.noreply.github.com>
2024-07-23 15:14:01 -0400

    Put identity back

--- pcs/testing/test_creator.py
+++ pcs/testing/test_creator.py
@@ -94,10 +94,10 @@ def createEvent(action:str, payload:dict=None, user:TestUser = None, userId:str=
         "fieldName":event['action'],
     }
 
-    # uid = str(uuid.uuid4())
-    # # Default user ID
-    # event["identity"]["sub"] = uid
-    # event["identity"]["claims"]["sub"] = uid
+    uid = str(uuid.uuid4())
+    # Default user ID
+    event["identity"]["sub"] = uid
+    event["identity"]["claims"]["sub"] = uid
 
     # if use user ID in event 
     if userId:
